c09aade63411e1b591b3b79332f42ebb68d5aa99
Sumit <timusumit@gmail.com>
2018-11-21 17:15:10 +0545

    done

--- application/views/site/products.php
+++ application/views/site/products.php
@@ -103,7 +103,7 @@
 			
 
 			<!-- Start faq Area -->
-			<section class="faq-area section-gap relative">
+		<!-- 	<section class="faq-area section-gap relative">
 				<div class="overlay overlay-bg"></div>
 				<div class="container">
 					<div class="row justify-content-center align-items-center">
@@ -140,20 +140,10 @@
 								</p>
 							</div>
 						</div>
-						<!-- <div class="col-lg-3 col-md-6">
-							<div class="single-faq">
-								<div class="circle">
-								  <div class="inner"></div>
-								</div>
-								<h5 class="counter">367</h5>
-								<p>
-									Tickets Submited
-								</p>
-							</div>
-						</div> -->																		
+																								
 					</div>
 				</div>	
-			</section>
+			</section> -->
 			<!-- End faq Area -->
 						
 			<!-- Start feedback Area -->
